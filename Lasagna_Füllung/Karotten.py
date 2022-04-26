3337a561206746cda847e2e7f980b347db32e71c
Willi321 <104350536+Willi321@users.noreply.github.com>
2022-04-26 14:07:27 +0200

    Datenbank Versuch

--- Lasagna_Füllung/Karotten.py
+++ Lasagna_Füllung/Karotten.py
@@ -1,7 +1,45 @@
 def main():
-    print("Hello World!")
+    print("Personenerkennung: ")
+def Funktion():
+ 
+ while True:   
+  def dateninput():
+    x = 0
+    Person = input("Vertreten sie eine Instuition? Ja / Nein")
+    if Person == "Ja": 
+        x =+1
+        return x
+    elif Person == "Nein":
+        x =+0
+        return x 
+    else:
+        print("Ungültige Antwort!")
+
+    y = y+1
+
+  def erkennung():
+        if x == 1 :
+            Institution
+        else:
+            NatuerlicheMenschen
+
+  if y == 5 :
+   break
+
+
+class NatuerlicheMenschen:
+        def natuerlicheMenschen(self, Vorname, Nachname, Geburtsdatum):
+            self.vorname = Vorname
+            self.nachname = Nachname
+            self.geburtsdatum = Geburtsdatum
+
+class Institution:
+        def institution(self, Firmenname, Firmentyp, Gründungsdatum):
+            self.firmenname = Firmenname
+            self.firmentyp = Firmentyp
+            self.Gruendungsdatum = Gründungsdatum
+
 
 if __name__ == "__main__":
-    main(
-        
-    )
+    main()
+    Funktion()   
